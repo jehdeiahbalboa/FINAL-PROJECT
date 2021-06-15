3ad812a626e379cd3cce53b7a2e21072d247dce8
jehdeiahbalboa <85625306+jehdeiahbalboa@users.noreply.github.com>
2021-06-15 18:52:38 +0800

    Add files via upload

---
+++ SUPER FINAL PROJECT.js
@@ -0,0 +1,11 @@
+const data = {
+    products:[
+    {id : 1, name : 'Dress1', price : 150 ,image: '/images/Jordan_1.jpg'},
+    {id : 2,name : 'Dress2', price : 100 ,image: '/images/Jordan_2.jpg'},
+    {id : 3,name : 'Dress3', price : 100 ,image: '/images/Jordan_3.jpg'},
+    {id : 4,name : 'Dress4', price : 500 ,image: '/images/Jordan_4.jpg'},
+    {id : 5,name : 'Dress5', price : 100 ,image: '/images/Jordan_5.jpg'},
+    {id : 6,name : 'Dress6', price : 100 ,image: '/images/Jordan_6.jpg'},
+]
+}
+export default data;
